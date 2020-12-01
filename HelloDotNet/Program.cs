073de4440f9101993453dbe4d84d230096e3ca83
Tushar443 <moretushart33@gmail.com>
2020-12-01 10:01:26 +0530

    1st DotNet Program

---
+++ HelloDotNet/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace HelloDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            demo1 d = new demo1();
+            d.P1=14;
+            
+            Console.WriteLine(d.P1);
+        }
+       // static void Main(string [] args)
+       // {
+
+       // }
+    }
+    class demo1
+    {
+        private int p1 ;
+            public int P1
+        {
+            set
+            {
+                if(value< 100)
+                {
+                    p1 = value;
+                }
+                else
+                {
+                    Console.WriteLine( "Invalid ");
+                }
+            }
+            get
+            {
+                return p1;
+            }
+        }
+    }
+}
+
+
+namespace PravinAndAnjaliAndKishor
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Pravin cha Lagna");
+        }
+    }
+}
---
+++ LockdownProject/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BaseClass baseClass = new BaseClass();
+            //baseClass.A = 12;
+            // baseClass.display();
+            subClass sub = new subClass();
+            sub.A = 12;
+            sub.B = 45;
+            sub.display();
+
+            Console.WriteLine("@@@@@@@@@@@@@@@@@@");
+
+            BaseClass sub1 = new subClass();  //Complie time polymorphism
+            subClass d2 = (subClass)sub1;
+
+            d2.A = 134;
+            d2.B = 45455;
+            d2.display();
+
+            
+           
+        }
+    }
+    class BaseClass
+    {
+        private int a;
+
+        public int A
+        {
+            set
+            {
+                a = value;
+            }
+            get
+            {
+                return a;
+            }
+        }
+
+        public virtual void display()
+        {
+            Console.WriteLine("A value is :" +a);
+        }
+    }
+
+    class subClass : BaseClass
+    {
+        private int b;
+        public int B
+        {
+            set
+            {
+                b = value;
+            }
+            get
+            {
+                return b;
+            }
+        }
+        public override void  display()
+        {
+            base.display();
+            Console.WriteLine("B value is : " + B);
+        }
+    }
+
+    class DefaultProperties
+    {
+        private int P
+        {
+            get;set;
+        }
+    }
+}
