c9ed0b27e3ca4af41dbab766edc62e763a216ca5
Tushar443 <moretushart33@gmail.com>
2020-12-05 17:20:38 +0530

    Sorted

---
+++ Day6_practice/Collection/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrList = new ArrayList();
+            arrList.Add(10);
+            arrList.Add("Tushar");
+            arrList.Add(1341.5);
+            arrList.Add(true);
+
+
+            arrList.Remove("Tushar");
+            arrList.RemoveAt(0);
+
+            foreach (var item in arrList)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Console.WriteLine("Size of ArrayList " +arrList.Count);
+            
+        }
+    }
+}
