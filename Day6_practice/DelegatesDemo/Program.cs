6f79123a681da00dc9b09a41820d0fd2a6175669
Tushar443 <moretushart33@gmail.com>
2020-12-06 10:21:49 +0530

    collection

--- Day6_practice/DelegatesDemo/Program.cs
+++ Day6_practice/DelegatesDemo/Program.cs
@@ -6,10 +6,55 @@ using System.Threading.Tasks;
 
 namespace DelegatesDemo
 {
+    public delegate void del1();
+    public delegate int delAdd(int i, int j);
     class Program
     {
         static void Main(string[] args)
         {
+            del1 objDell = new del1(Dispaly);
+            //objDell();
+
+            objDell += new del1(Show);
+            objDell();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            //objDell = Dispaly;
+            objDell += Show;
+            //objDell -= Dispaly;
+
+            //objDell -= Show;
+            // objDell();
+            //objDell += new del1(Show);
+
+            //objDell();
+            //delAdd ibjAdd = new delAdd(Addition);
+            //int sum = ibjAdd(5, 8);
+            //Console.WriteLine(sum);
+
+            del1 objectNew = (del1)Delegate.Combine(new del1(Show), new del1(Dispaly), new del1(Dispaly), new del1(Show));
+            // objectNew();
+            //objectNew = (del1)Delegate.Remove(objectNew, new del1(Dispaly));
+            objectNew = (del1)Delegate.RemoveAll(objectNew, new del1(Dispaly));
+
+            objectNew();
+
+        }
+        public static void Dispaly()
+        {
+            Console.WriteLine("Display");
+        }
+
+        public static void Show()
+        {
+            Console.WriteLine("Show");
+        }
+        public static int Addition(int x, int y)
+        {
+            return x + y;
         }
     }
 }
---
+++ Day6_practice/Delegates_Class/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Myclass myClass = new Myclass();
+            //myDell dellObj = new myDell(myClass.Add);
+            //dellObj += new myDell(myClass.sub);
+            //Console.WriteLine(dellObj(3,5));
+
+            Console.WriteLine(myClass.callMethod(myClass.Add, 3, 6));
+            Console.WriteLine(myClass.callMethod(myClass.sub, 3, 6)) ;
+            Console.WriteLine(myClass.callMethod(myClass.mul, 3, 6)) ;
+            Console.WriteLine(myClass.callMethod(myClass.Add, 3, 6));
+
+        }
+    }
+
+    public delegate int myDell(int x ,int y);
+    class Myclass
+    {
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+        public int sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+        public int callMethod(myDell objDell,int a,int b)
+        {
+            return objDell(a, b);
+        }
+    }
+}
